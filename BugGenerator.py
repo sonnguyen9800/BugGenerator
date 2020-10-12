2dde89469050062e3898de51b4a173e1d4aee653
sonnguyen9800 <sonnguyen9800@gmail.com>
2020-10-12 15:30:03 +0700

    Edit and upgrade

--- BugGenerator.py
+++ BugGenerator.py
@@ -2,22 +2,14 @@ import string
 import random
 from time import sleep
 import argparse
-import cv2
 import threading as th
-
+from tqdm import tqdm, trange
+from colorama import Fore, Style, init
+from art import *
 keep_going = True
 
 
-ap = argparse.ArgumentParser()
-ap.add_argument('-f', '--flag', required=True, help='path to port number from Left Camera')
-args = ap.parse_args()
-
-flag = args.flag
-
-if (str(flag) == "true"):
-    flag = True
-elif (str(flag) == "false"):
-    flag = False
+state = 0
 
 
 class bcolors:
@@ -33,18 +25,19 @@ class bcolors:
 def key_capture_thread():
     global keep_going
     input()
-    global flag
-    if flag == True:
-        flag = False
-    elif flag == False:
-        flag = True
+    global state
+    if state == 0:
+        state = 1
+    elif state == 1:
+        state = 2
+    elif state == 2:
+        state = 3
 
 
 def do_stuff():
     th.Thread(target=key_capture_thread, args=(), name='key_capture_thread', daemon=True).start()
     while keep_going:
         print('still going...')
-
 
 if __name__ == '__main__':
     str = ""
@@ -84,9 +77,10 @@ if __name__ == '__main__':
 
     ]
 
+    
     while True:
 
-        if flag==False:
+        if state==0:
             th.Thread(target=key_capture_thread, args=(), name='key_capture_thread', daemon=True).start()
             exeption = exceptionList[random.randint(0, len(exceptionList)-1)]
             print(bcolors.FAIL+ "Throw Exception, ERROR: Serious troubles occurs:" +exeption.__name__  + bcolors.ENDC)
@@ -99,7 +93,7 @@ if __name__ == '__main__':
 
             sleep(time / 1000)
 
-        elif flag == True:
+        elif state == 1:
 
             th.Thread(target=key_capture_thread, args=(), name='key_capture_thread', daemon=True).start()
 
@@ -110,7 +104,16 @@ if __name__ == '__main__':
             length = random.randint(0, 150)
             time = random.randint(0, 300)
             str = ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(length))
-            #print(bcolors.FAIL + str + bcolors.ENDC)
-
             sleep(time / 1000)
+            
+        elif state == 2:
+            for i in trange(int(100), bar_format="{l_bar}%s{bar}%s{r_bar}" % (Fore.GREEN, Fore.RESET)):
+                sleep(0.01)
+                pass 
+            print(bcolors.OKGREEN + "EVERYTHING IS GOOD" )
+            tprint( "         SUCCESS", font="colossal" )
+            print( bcolors.ENDC)
+            break
+
+exit()
 
